1e1999fdd44709bce72299ad1504b7d046045cd7
jacksonterrill <jacksonterrill3@gmail.com>
2022-04-13 15:56:02 -0400

    create db

---
+++ create-f1db.sql
@@ -0,0 +1,8 @@
+-- f1db - Formula 1 Database
+-- Jackson Terrill and Brady Hobson
+-- Creates the f1db schema and its tables, and inserts values into the database
+
+DROP DATABASE IF EXISTS f1db;
+CREATE DATABASE IF NOT EXISTS f1db;
+USE f1db;
+
